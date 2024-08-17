a3a4743398fc4439fcefb98a9afaeafb0af6a0e6
deontaeramsey <1deontae.ramsey@gmail.com>
2024-08-17 15:03:18 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -25,8 +25,8 @@ def mult(a, b):
 # Divide function
 # a -- dividend
 # b -- divisor
-#def div(a, b):
-#    return a / b
+def div(a, b):
+    return a / b
 
 
 # -------------------------------------------------------- #
